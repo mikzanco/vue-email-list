b37ee1489c3d046e834fa0a774ad87eddea6ec3e
mikzanco <mikyzancolo97@gmail.com>
2022-11-14 09:43:59 +0100

    fix push in page, tutte e 10 le email vengono pushate in un ul-li con un v-for

--- js/script.js
+++ js/script.js
@@ -15,14 +15,16 @@ createApp({
         // inserisco la url dell'API,
         // creo arrpw function nel then che risponde quando lo stato della chimata è positivo e da quindi 200 
         this.emailList = [];
+        for(let i = 1; i <= 10; i++){
+           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( result =>{
+             console.log(result.data);
+             this.emailList.push(result.data.response);
+            })
+        // return this.getApi(++this.count);
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then( risultato =>{
-          console.log(risultato.data);
-          this.emailList.push(result.data.response);
-
-        })
-        return this.getApi(++this.count);
+        }
+        
       }
   },
   mounted(){
