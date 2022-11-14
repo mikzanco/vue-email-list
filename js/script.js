43f4f22c430e6e58982e7c254e68a48b8746d831
mikzanco <mikyzancolo97@gmail.com>
2022-11-14 09:30:39 +0100

    fix last push

--- js/script.js
+++ js/script.js
@@ -1,33 +1,76 @@
-const {createApp } = Vue;
-
+const { createApp } = Vue;
 
 createApp({
 
   data(){
-    return{
+    return {
       title: 'Ajax',
-      email: '',
+      emailList: '',
+
     }
+    
   },
   methods:{
-    getApi(){
+      getApi(){
+        // inserisco la url dell'API,
+        // creo arrpw function nel then che risponde quando lo stato della chimata è positivo e da quindi 200 
+        this.emailList = [];
 
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then( risultato => {
-        this.email = risultato.data.response
-      })
-      .catch( errore => {
-        this.title = errore.code
-        console.log(errore.code);
-      })
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( risultato =>{
+          console.log(risultato.data);
+          this.emailList.push(result.data.response);
 
-    }
-      
+        })
+        return this.getApi(++this.count);
+      }
   },
   mounted(){
-    
     this.getApi();
-    
-  }
+  },
+  created(){
 
+  }
 }).mount('#app');
+
+
+
+
+
+
+
+
+// const {createApp } = Vue;
+
+
+// createApp({
+
+//   data(){
+//     return{
+//       // title: 'Ajax',
+//       // email: '',
+//     }
+//   },
+//   methods:{
+//     getApi(){
+
+//       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//       .then( risultato => {
+//         console.log(risultato);
+//         // this.email = risultato.data.response
+//       })
+//       // .catch( errore => {
+//       //   this.title = errore.code
+//       //   console.log(errore.code);
+//       // })
+
+//     }
+      
+//   },
+//   mounted(){
+    
+//     this.getApi();
+    
+//   }
+
+// }).mount('#app');
